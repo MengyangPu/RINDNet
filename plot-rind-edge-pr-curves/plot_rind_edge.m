68b38778489e87e73f5dbbbc03f499b5331a1137
Mengyang Pu <16120407@bjtu.edu.cn>
2021-11-23 10:43:57 +0800

    Update plot_rind_edge.m

--- plot-rind-edge-pr-curves/plot_rind_edge.m
+++ plot-rind-edge-pr-curves/plot_rind_edge.m
@@ -15,12 +15,18 @@ colors = {
     [0.0392, 0.4275, 0.2667]
     [0.4157, 0.5373, 0.0824]
     [0.5490, 0.5490, 0.4549]
+    [0.4471, 0.3333, 0.1725]
+    [1.0000, 0.0000, 0.0000]
+    [0.7059, 0.5333, 0.8824]
 };
 
-lines = {'-','-','-','-','-','-','-','--','--','--','--','--','--','--'};
+lines = {'-','-','-','-','-','-','-','-','-','--','--','--','--','--','--','--','--'};
 
 names = {
-    'RINDNet'
+    'RINDNet-ResNeXt101'
+    'RINDNet-ResNet101'
+    'RINDNet-ResNet50'
+    'RINDNet-ResNeXt50'  
     'DFF'
     'RCF'
     'CASENet'
@@ -37,25 +43,34 @@ names = {
 };
 
 years = {
-    ' (2020)'
+    ' (2021)'
+    ' (2021)'
+    ' (2021)'
+    ' (2021)'
+    ' (2019)'
+    ' (2017)'
+    ' (2017)'
+    ' (2019)'
+    ' (2019)'
+    ' (2019)'
+    ' (2018)'
+    ' (2015)'
     ' (2019)'
     ' (2019)'
     ' (2018)'
     ' (2017)'
-    ' (2017)'
-    ' (2015)'
-    ' (2015)'
+    ' (2020)'
 };
 
 edgesEvalPlot('eval_depth', names, colors, lines, years, true);
-% saveas(gcf,'depth.png');
-% close all
+saveas(gcf,'depth.png');
+close all
 edgesEvalPlot('eval_normal', names, colors, lines, years, true);
-% saveas(gcf,'normal.png');
-% close all
+saveas(gcf,'normal.png');
+close all
 edgesEvalPlot('eval_reflectance', names, colors, lines, years, true);
-% saveas(gcf,'reflectance.png');
-% close all
+saveas(gcf,'reflectance.png');
+close all
 edgesEvalPlot('eval_illumination', names, colors, lines, years, true);
-% saveas(gcf,'illumination.png');
-% close all
+saveas(gcf,'illumination.png');
+close all
